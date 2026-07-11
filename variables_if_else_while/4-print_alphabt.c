884c79e529c562ae7aea426bbbaf79ea9a1e5e10
grinskey-com <4zw5nh5f4k@privaterelay.appleid.com>
2026-07-11 14:28:54 +1000

    add c file

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet, excluding q and e
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+	putchar('\n');
+	return (0);
+}
