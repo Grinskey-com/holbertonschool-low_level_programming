85b197e137fbca1733b57693076f361e3661ce12
grinskey-com <4zw5nh5f4k@privaterelay.appleid.com>
2026-07-12 17:05:42 +1000

    task 11 files

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -9,6 +9,7 @@ int _putchar(char c);
 int _islower(int c);
 int print_last_digit(int);
 int print_sign(int n);
+void print_to_98(int n);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 void jack_bauer(void);
