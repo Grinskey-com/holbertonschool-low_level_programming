9756b978a2bcb61097f824bdfe93609cc5d2587f
grinskey-com <4zw5nh5f4k@privaterelay.appleid.com>
2026-07-12 19:30:50 +1000

    triangles

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a right-aligned triangle of a given size
+ * @size: the size of the triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < (size - i - 1); j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j < (i + 1); j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
