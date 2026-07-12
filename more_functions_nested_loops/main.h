fe5af0d3e5a4ba04579fe1c68912306b8b9cd1aa
grinskey-com <4zw5nh5f4k@privaterelay.appleid.com>
2026-07-12 18:39:06 +1000

    Task 0 for uppercase

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+/* main.h */
+#ifndef MAIN_H
+#define MAIN_H
+
+int add(int, int);
+int _abs(int);
+int _isupper(int c);
+int _isalpha(int c);
+int _putchar(char c);
+int _islower(int c);
+int print_last_digit(int);
+int print_sign(int n);
+void print_to_98(int n);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void jack_bauer(void);
+void times_table(void);
+
+#endif
