edce01088a321b6306e96923f773a54771bf59b5
grinskey-com <4zw5nh5f4k@privaterelay.appleid.com>
2026-07-15 22:09:48 +1000

    debugging and added if statement clarity

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -6,27 +6,46 @@
  *
  */
 
-int main() 
+int main(void) 
 {
 	int choice;
 	
 	
 	do 
 	{
-	//Display calculator menu	
-	printf("\nWelccome to the calculator app!\n");
-	printf("1\) Add\n");
-	printf("2\) Subtract\n");
-	printf("3\) Multiply\n");
-	printf("4\) Divide\n");
-	printf("0\) Quit\n");
+		//Display calculator menu	
+		printf("\nWelccome to the calculator app!\n");
+		printf("1. Add\n");
+		printf("2. Subtract\n");
+		printf("3. Multiply\n");
+		printf("4. Divide\n");
+		printf("0. Quit\n");
 
-	if (choice == 0)
+		//Input from user
+		//Ensure input used is an interger
+		if (scanf("%d", &choice) != 1)
 		{
-		printf("Bye for now!\n")	
+			printf("Invalid input. Please enter a number.\n");
+			//Clear invalid input
+			while (getchar() != '\n')//clear input
+ 			continue;
+		}	
+
+		//next check the interger is within the option range 0-4
+		if (choice < 0 || choice > 4)
+		{
+			printf("invalid niput. Please enter a number from 0-4.\n");
+			continue;
+		}
+
+
+		if (choice == 0)
+		{
+			printf("Bye for now!\n");	
  		}
 
-	} while(choice != 0);//keep looping until exit is selcted
+
+	} while(choice != 0);//Keep looping until exit is selected
 		
-	return(0)	
+	return(0);	
 }		
