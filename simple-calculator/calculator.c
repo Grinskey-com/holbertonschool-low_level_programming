eb747783be74ce87c61da5dac9e97806ed5a5afb
grinskey-com <4zw5nh5f4k@privaterelay.appleid.com>
2026-07-16 20:22:35 +1000

    add add program

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -9,13 +9,14 @@
 int main(void) 
 {
 	int choice;
-	
-	
+	double a, b;
+	double result;
+
 	do 
 	{
 		/*Display calculator menu*/	
-		printf("\nWelccome to the calculator app!\n");
-		printf("1. Add\n");
+		printf("\nWelcome to the calculator app!\n");
+	 	printf("1. Add\n");
 		printf("2. Subtract\n");
 		printf("3. Multiply\n");
 		printf("4. Divide\n");
@@ -26,25 +27,36 @@ int main(void)
 		{
 			printf("Invalid input. Please enter a number.\n");
 			/*Clear invalid input*/
-			while (getchar() != '\n')/*clear input*/
- 			continue;
+			while (getchar() != '\n')
+				continue;
 		}	
 
 		/*next check the interger is within the option range 0-4*/
 		if (choice < 0 || choice > 4)
-		{
+		{	
 			printf("invalid niput. Please enter a number from 0-4.\n");
 			continue;
 		}
 
-
-		if (choice == 0)
+		/*Addition program*/		
+		if (choice == 1)
 		{
+			printf("A: ");
+			scanf("%lf", &a);
+			printf("B: ");
+			scanf("%lf", &b);
+
+			result = a + b;
+			printf("Result: %.2f\n", result);
+
+		}
+		if (choice == 0)
+			{	
 			printf("Bye for now!\n");	
- 		}
+ 			}
 
 
-	} while(choice != 0);/*Keep looping until exit is selected*/
+} while(choice != 0);/*Keep looping until exit is selected*/
 		
 	return(0);	
 }		
