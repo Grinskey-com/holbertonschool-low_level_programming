ae2570057baf9fe253108e82eda2c592edf2e8d5
grinskey-com <4zw5nh5f4k@privaterelay.appleid.com>
2026-07-18 23:50:26 +1000

    puts2 make sure you check for null

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: pointer to the string to print
+ */
+void puts2(char *str)
+{
+	int i;
+
+	/* Start at 0, check for the null terminator, hop by 2 */
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
