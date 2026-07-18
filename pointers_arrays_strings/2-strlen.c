61a3667036bb600d095a1a5624005b16f71fa435
grinskey-com <4zw5nh5f4k@privaterelay.appleid.com>
2026-07-18 18:29:48 +1000

    task 3 string length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string to check
+ *
+ * Return: the length of the string as an integer
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		/* Loop increments i until it hits '\0'*/
+	}
+
+	return (i);
+}
