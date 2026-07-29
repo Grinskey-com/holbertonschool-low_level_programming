c537a9bcb3a71830b804aafe1d1db2d7b861b3c2
grinskey-com <4zw5nh5f4k@privaterelay.appleid.com>
2026-07-29 20:58:52 +1000

    was it worth it?

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,13 +2,15 @@
 #define FUNCTION_POINTER_H
 
 /**
- *
- *
- *
- *
+ * file: function_pointers.h
+ * desc: Header file containing function prototypes
  */
 
-/* Function prototypes */
+/**
+ * print_name - prints a name using a given function pointer
+ * @name: string to print
+ * @f: pointer to the printing function
+ */
 void print_name(char *name, void (*f)(char *));
 
-#endif
+#endif /* FUNCTION_POINTER_H */
