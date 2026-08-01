753bab37a4fa876611ee70d241c3eb63b73356ed
grinskey-com <4zw5nh5f4k@privaterelay.appleid.com>
2026-08-01 13:38:57 +1000

    you can do anything

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all args inlc the first one
+ * @argc: the number of command-line arguments
+ * @argv: an array of pointers to the command-line arguments
+ *
+ * return: always 0 (success).
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+		
+	for(i = 0; i < argc; i++)
+	{
+		printf("argv[%d] = %s\n", i, argv[i]);
+	}
+
+	return (0);
+}
+
