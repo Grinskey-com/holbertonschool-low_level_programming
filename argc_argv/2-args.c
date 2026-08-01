89f277f1fcd64eaebd24211f4c3a5f9917276e5f
grinskey-com <4zw5nh5f4k@privaterelay.appleid.com>
2026-08-01 13:51:00 +1000

    betty likes to arg

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - prints all args inlc the first one
- * @argc: the number of command-line arguments
- * @argv: an array of pointers to the command-line arguments
+ * main - Prints all args including the first one.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of pointers to the command-line arguments.
  *
- * return: always 0 (success).
+ * Return: Always 0 (success).
  */
 int main(int argc, char *argv[])
 {
 	int i;
 
-	for(i = 0; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
 
 	return (0);
 }
-
