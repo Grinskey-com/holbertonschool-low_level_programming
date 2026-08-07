609625a19b9bf4f7c8785b7e4503c145893f7caa
grinskey-com <4zw5nh5f4k@privaterelay.appleid.com>
2026-08-07 12:55:57 +1000

    list len betty

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- *  st_len- Prints all the elements of a list_t list.
+ * list_len- count of the nomber of nodes in list.
  * @h: Pointer to the head of the list_t list.
  *
  * Return: The number of nodes in the list.
@@ -18,7 +18,7 @@ size_t list_len(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			
+
 		count++; /* increment count */
 		h = h->next; /* point to next node */
 	}
